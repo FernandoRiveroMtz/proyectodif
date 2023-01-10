3e15b8dc023ca6be39dadd9dbfe6b875455bf885
FernandoRiveroMtz <115180038+FernandoRiveroMtz@users.noreply.github.com>
2023-01-10 08:57:49 -0600

    Fernando

--- resources/views/partials/nav.blade.php
+++ resources/views/partials/nav.blade.php
@@ -1,4 +1,4 @@
-<!--  <nav class="navbar navbar-expand-lg navbar-dark bg-gob">
+<!--  <nav class="navbar navbar-expand-lg navbar-dark bg-gob">0
     <div class="container">
       <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
