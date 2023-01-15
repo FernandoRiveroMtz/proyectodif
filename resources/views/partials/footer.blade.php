ab5a262ea0633f10dac6ea29a52438b9e9746bcf
FernandoRiveroMtz <115180038+FernandoRiveroMtz@users.noreply.github.com>
2023-01-14 18:31:30 -0600

    Fernando
    Agregación de color DIF al estilo de navbar para que este disponible de forma global y he colocado la clase en los diferentes elementos que mandaban a llamar al color primary de bootstrap

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -1,4 +1,4 @@
-<footer class="bg-primary text-center text-dark-start">
+<footer class="coldif text-center text-dark-start">
   <!-- Grid container -->
   <div class="container p-4">
     <!--Grid row-->
@@ -26,6 +26,34 @@
       </div>
     </div>
   </div>
+
+  <!-- Footer de prueba -->
+  
+  <div class="container ">
+  <div class="row justify-content-md-center">
+    <div class="col col-lg-2">
+      <img src="{{asset('img/dif.png')}}" width="145">
+    </div>
+    <div class="col-md-auto">
+    <hr style="background-color: white; border-color:white; color:white; transform:rotatez(90deg); width:9vw; position:block; height: 7px; margin-top: 4.4%; " class="d-none d-xxl-block">
+          
+    </div>
+    <div class="col col-lg-2">
+    <a class="btn btn-primary btn-floating m-1" href="https://www.youtube.com/@difecatepec5605" target="_blank" role="button"><i class="bi bi-youtube" style="color: white; font-size: 1.3rem"></i></a>
+          <!-- Facebook -->
+          <a class="btn btn-primary btn-floating m-2" href="https://es-la.facebook.com/SistemaMunicipalDIFEcatepec/" target="_blank" role="button"><i class="bi bi-facebook" style="color: white; font-size: 1.3rem"></i></a>
+          <!-- Twitter -->
+          <a class="btn btn-primary btn-floating m-1" href="https://twitter.com/ecatepecdif" target="_blank" role="button"><i class="bi bi-twitter" style="color: white; font-size: 1.3rem"></i></a>
+          <!-- Instagram -->
+          <a class="btn btn-primary btn-floating m-1" href="https://www.instagram.com/dif_ecatepec/?hl=es" target="_blank" role="button"><i class="bi bi-instagram" style="color: white; font-size: 1.3rem"></i></a>
+          <!-- Tik Tok -->
+          <a class="btn btn-primary btn-floating m-1" href="https://www.tiktok.com/@difecatepec" target="_blank" role="button"><i class="bi bi-tiktok" style="color: white; font-size: 1.3rem"></i></a>
+        
+    </div>
+  </div>
+
+   <!-- Fin footer de prueba -->
+      
   <!-- Copyright -->
   <div class="text-center p-3" style="background-color: rgba(0, 0, 0, 0.5); color:white;">
     © 2022-2024 Copyright: <span></span>
