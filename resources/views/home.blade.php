a5e06ebca283407ebd3cab47c4a1d6aec569f1aa
FernandoRiveroMtz <115180038+FernandoRiveroMtz@users.noreply.github.com>
2023-01-24 08:49:57 -0600

    Fernando
    Adición del apartado ¿Quiénes somos?

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -20,21 +20,13 @@
 
                 <p class="coldif img-fluid" style="color: white; width: 330px; font-size: xx-large;">¿Quienes somos?</p>
 
-                <p>Descripción completa solo texto, para tener información para los usuarios.</p>
-                <p style="margin: 20px; line-height: .9em;">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Dolore similique doloribus, molestias illum reiciendis facere expedita</p>
-                <p style="margin: 20px; line-height: .9em;">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Dolore similique doloribus, molestias illum reiciendis facere expedita</p>
-                <p style="margin: 20px; line-height: .9em;">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Dolore similique doloribus, molestias illum reiciendis facere expedita</p>
-                <p style="margin: 20px; line-height: .9em;">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Dolore similique doloribus, molestias illum reiciendis facere expedita</p>
-                <p style="margin: 20px; line-height: .9em;">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Dolore similique doloribus, molestias illum reiciendis facere expedita</p>
-                <p style="margin: 20px; line-height: .9em;">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Dolore similique doloribus, molestias illum reiciendis facere expedita</p>
-                <p style="margin: 20px; line-height: .9em;">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Dolore similique doloribus, molestias illum reiciendis facere expedita</p>
-                <p style="margin: 20px; line-height: .9em;">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Dolore similique doloribus, molestias illum reiciendis facere expedita</p>
-                <p style="margin: 20px; line-height: .9em;">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Dolore similique doloribus, molestias illum reiciendis facere expedita</p>
-                <p style="margin: 20px; line-height: .9em;">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Dolore similique doloribus, molestias illum reiciendis facere expedita</p>
-
+                <!-- <p>Descripción completa solo texto, para tener información para los usuarios.</p> -->
+                <p style="margin: 20px; line-height: .9em; text-align: justify;">El Sistema para el Desarrollo Integral de la Familia de Ecatepec de Morelos, es un Organismo Público Descentralizado, nuestra misión es administrar, coordinar, planear y gestionar los diferentes apoyos y programas de carácter social de manera eficiente, con calidad de transparencia para la consolidación de las familias ecatepenses; comprometidos con promover y supervisar los programas de asistencia social en beneficio de los sectores más vulnerables de nuestro municipio.</p>
+                <p style="margin: 20px; line-height: .9em; text-align: justify;">Impulsamos politicas de asistencia social para el desarrollo de las comunidades, con el propósito de erradicar el origen y efectos de los grupos vulnerables en coordinacion con el H. Ayuntamiento de Ecatepec de Morelos. Operamos un amplio esquema de trabajo que va de la mano con los distintos programas que permiten dar mayor cobertura a las necesidades de la población.</p>
+                <p style="margin: 20px; line-height: .9em; text-align: justify;">Nuestro compromiso es atender de manera inmediata otorgando servicios de calidad, que mejoren las condiciones de vida de los sectores más desprotegidos, tomando en cuenta los parámetros de desarrollo en el que se desenvuelven tanto adultos, como niños, niñas y adolecentes.</p>
                 
             </div>
-            {{-- </div> --}}
+            
         </div>
 
         <div class="col-md-12">
